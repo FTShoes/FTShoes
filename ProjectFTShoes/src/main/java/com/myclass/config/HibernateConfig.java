995c4332a1acf833fc5a9be4e6335725ed13170b
HaseNguyen <46945964+HaseNguyen@users.noreply.github.com>
2019-11-25 14:16:30 +0700

    Project
    ConnectDataBase

---
+++ ProjectFTShoes/src/main/java/com/myclass/config/HibernateConfig.java
@@ -0,0 +1,48 @@
+package com.myclass.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource source = new DriverManagerDataSource();
+		source.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		source.setUrl("jdbc:mysql://localhost:3306/ftshoes");
+		source.setUsername("root");
+		source.setPassword("123456");
+		return source;
+	}
+	@Bean
+
+	public LocalSessionFactoryBean sessionFactoryBean() {
+		LocalSessionFactoryBean bean = new LocalSessionFactoryBean();
+		bean.setDataSource(dataSource());
+		bean.setPackagesToScan("com.myclass.entity");
+		
+		Properties properties = new Properties();
+		properties.put("hiberate.dialect","org.hibernate.dialect.MySQLDialect");
+		properties.put("hiberate.show_sql",true);
+		properties.put("hiberate.format_Sql",true);
+		
+		return bean;
+	}
+	@Bean
+	public HibernateTransactionManager transactionManager() {
+		HibernateTransactionManager manager = new HibernateTransactionManager();
+		manager.setSessionFactory(sessionFactoryBean().getObject());
+		
+		return manager;
+	}
+}
---
+++ ProjectFTShoes/src/main/java/com/myclass/config/WebMvcConfig.java
@@ -0,0 +1,36 @@
+package com.myclass.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//import com.myclass.repository.RoleRepository;
+//import com.myclass.repository.impl.RoleReponsitotyImpl;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.myclass")
+public class WebMvcConfig implements WebMvcConfigurer {
+
+public void addCorsMappings(CorsRegistry registry) {
+	registry.addMapping("/api/**")
+	.allowedOrigins("*")
+	.allowCredentials(false)
+	.maxAge(4800);
+}
+	
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		
+		registry
+			.addResourceHandler("swagger-ui.html")
+			.addResourceLocations("classpath:/META-INF/resources/");
+		
+		registry
+			.addResourceHandler("/webjars/**")
+			.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+}
